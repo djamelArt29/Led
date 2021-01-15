12202c81507f6006ccc433df875059ede1d1ace3
Djamel <ahnouchedjamel29@gmail.com>
2021-01-15 22:20:37 +0100

    calculator

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        Button btnSum= (Button)findViewById(R.id.btnSum);
+        TextView resul= (TextView)findViewById(R.id.tVResult);
+        EditText eTVal1 = (EditText)findViewById(R.id.eTVal1);
+        EditText eTVal2 = (EditText)findViewById(R.id.etVal2);
+
+
+        btnSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                int sum = 0;
+                int val1= Integer.parseInt(eTVal1.getText().toString());
+                int val2=Integer.parseInt(eTVal2.getText().toString());
+                sum = val1+ val2;
+
+                resul.setText(String.valueOf(sum));
+                eTVal1.setText("0");
+                eTVal2.setText("0");
             }
         });
     }
